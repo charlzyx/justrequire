1773215aa4e3dd70655dee4ca16095ae9fe04eab
xiaochao.yang <xiaochao.yang@relxtech.com>
2022-03-11 09:34:18 +0800

    fix: 🐛 type defines

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,3 @@
-declare module 'justrequire' {
-  <R = any>(
-    /**
-     * @description 模块名称或者文件地址
-     **/
-    moduleName: string,
-    /**
-     * @description 文件查找的前缀
-     * @default 默认 process.cwd()
-     **/
-    prefix?: string
-  ) => R
-}
+export declare function justrequire<R = any>(moduleName: string, pathPrefix?: string): R;
+
+export default justrequire ;
